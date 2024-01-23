b4fa129ccef0c7eae6fdf5a761aa6cc58e887c4b
Jon Nicholson <jonnicholson@Jons-MacBook-Pro.local>
2024-01-23 12:59:01 +0000

    Fixed some build errors

--- components/dashboard/dashboardLoader.tsx
+++ components/dashboard/dashboardLoader.tsx
@@ -1,12 +1,20 @@
+
+import { useState } from "react";
+
+import { FilterOptions } from "@/lib/types/task";
+
 import DashboardFilter from "./dashboardFilter";
 import DashboardHeader from "./dashboardHeader";
 import DashboardSkeletonItem from "./dashboardSkeletonItem";
 
 export default function DashboardLoader() {
+
+    const [active, setActive] = useState<FilterOptions>("All")
+
     return (
         <>
             <DashboardHeader />
-            <DashboardFilter />
+            <DashboardFilter active={active} setActive={setActive} />
             <DashboardSkeletonItem />
             <DashboardSkeletonItem />
             <DashboardSkeletonItem />
